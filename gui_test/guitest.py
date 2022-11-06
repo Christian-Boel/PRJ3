7e5b4cbc6d2d144714ab591872ad8687d64e4cf3
boel <christian.boel,poulsen@gmail.com>
2022-11-06 16:42:42 +0100

    Major improvements to homescreen

--- gui_test/guitest.py
+++ gui_test/guitest.py
@@ -1,65 +1,71 @@
 import tkinter as tk
 from PIL import ImageTk,Image
 
-window = tk.Tk()
+root = tk.Tk()
 
+buttonColor = "#2596be"
+headerFont = 'Arial 75 bold'
+buttonFont = 'Arial 15'
 
-defaultcolor = "#2596be"
-
-window.title("SPLASH")
-window.geometry("800x540")
-#window.configure(background="white")
+root.title("SPLASH")
+root.geometry("800x480")
+#root.configure(background="white")
 
 
 #EDIT PICTURE PATH!
-my_img = ImageTk.PhotoImage(Image.open("/Users/christian/PRJ3/gui_test/icons/drinkicon.png"))
-my_label = tk.Label(image=my_img)
-#my_label.pack()
 
+home_img = ImageTk.PhotoImage(Image.open(r"C:\Users\boel\PRJ3\PRJ3\gui_test\icons\white_homeicon_80x80.png"))
+home_button = tk.Button(image=home_img)
+
+back_img = ImageTk.PhotoImage(Image.open(r"C:\Users\boel\PRJ3\PRJ3\gui_test\icons\arrow_original_80x80.png"))
+back_button = tk.Button(image=back_img)
 
 Header = tk.Label(
     text="SPLASH",
-    font=('Arial',75),
+    font=headerFont,
     foreground="black")
 
-def mixDrinkButtonClicked():
-    myLabel = tk.Label(window,text="Mix drink clicked!")
-    myLabel.pack()
 
+def homescreenBtnPressed(btn): #Button pressed while on homescreen
+    match btn:
+        case "mix":
+            mixDrinkMenu()
+            #myLabel = tk.Label(root,text="mix drink clicked!")
+            #myLabel.pack()
+        case "add":
+            #addDrinkMenu()
+            myLabel = tk.Label(root,text="Add drink clicked!")
+            myLabel.pack()
+
+        case "remove":
+            #removeDrinkMenu()
+            myLabel = tk.Label(root,text="Remove drink clicked!")
+            myLabel.pack()
+
+
+#define buttons
 mixDrinkButton = tk.Button(
     text="MIX DRINK",
+    font = buttonFont,
     fg="black",
-    bg=defaultcolor,
-    # width = 25,
-    # height = 25,
-    padx = 10,
-    command=mixDrinkButtonClicked
+    bg=buttonColor,
+    command=lambda: homescreenBtnPressed("mix")
 )
-
-def addDrinkButtonClicked():
-    myLabel = tk.Label(window,text="Add drink clicked!")
-    myLabel.pack()
-
-removeDrinkButton = tk.Button(
-    text="ADD DRINK",
-    fg="black",
-    bg=defaultcolor,
-    # width = 25,
-    # height = 25,
-    command=addDrinkButtonClicked
-)
-
-def removeDrinkButtonClicked():
-    myLabel = tk.Label(window,text="Remove drink clicked!")
-    myLabel.pack()
 
 addDrinkButton = tk.Button(
     text="REMOVE DRINK",
+    font = buttonFont,
     fg="black",
-    bg=defaultcolor,
-    # width = 25,
-    # height = 25,
-    command=removeDrinkButtonClicked
+    bg=buttonColor,
+    command=lambda: homescreenBtnPressed("add")
+)
+
+removeDrinkButton = tk.Button(
+    text="ADD DRINK",
+    font = buttonFont,
+    fg="black",
+    bg=buttonColor,
+    command=lambda: homescreenBtnPressed("remove")
 )
 
 # entry = tk.Entry(
@@ -70,12 +76,23 @@ addDrinkButton = tk.Button(
 
 #homescreen
 
-def homescreen():
-    Header.place(rely= 0.1, relx=0.3)
+
+def clearScreen():
+    for widget in root.winfo_children():
+        widget.place_forget()
+
+def homeMenu():
+    clearScreen()
+    Header.place(rely= 0.05, relx=0.25)
     mixDrinkButton.place(rely = 0.4,relx=0.075, height = 200, width = 200)
     addDrinkButton.place(rely = 0.4,relx=0.375, height = 200, width = 200)
     removeDrinkButton.place(rely = 0.4,relx=0.675, height = 200, width = 200)
-    
 
-homescreen()
-window.mainloop()
+def mixDrinkMenu():
+    clearScreen()
+    home_button.place(y = 400,x = 0)
+    back_button.place(y=400,x = 720)
+
+#run program - start in home menu
+homeMenu()
+root.mainloop()
