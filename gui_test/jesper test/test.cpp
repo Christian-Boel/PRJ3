9c5b624c1fdd5a2b909c8850206f0514584504c9
jolle1235 <94052605+jolle1235@users.noreply.github.com>
2022-11-14 10:35:44 +0100

    mere test

--- gui_test/jesper test/test.cpp
+++ gui_test/jesper test/test.cpp
@@ -1,9 +1,13 @@
 #include "test.py"
 #include <pybind1/pybind11.h>
+#include <iostream>
 
 namespace py = pybind11;
+using namespace std;
 
 void main(){
+    py::scoped_interpreter python;
     
-    
+    auto printVariabel = py::module::import("sendFunction");
+    cout << printVa
 }
