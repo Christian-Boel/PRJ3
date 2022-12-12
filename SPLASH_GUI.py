62ee43edf842c83e3c5dd8c080f43db0df6b5d87
Christian-Boel <christian.boel.poulsen@gmail.com>
2022-12-12 13:34:21 +0100

    a

--- SPLASH_GUI.py
+++ SPLASH_GUI.py
@@ -296,10 +296,11 @@ def glassRegistered():
     try:
         file = os.open("/dev/spi_drv0", os.O_RDWR)
         SPI_Status = (os.read(file,16))
-        print("SPI: status read: " , SPI_Status.decode())
+        SPI_Decoded = SPI_Status.decode()
+        print("SPI: status read:" , SPI_Decoded))
     except:
         print("Failed to read from SPI")
-    return int(SPI_Status.decode(),base=10)
+    return int(SPI_Decoded,base=10)
 
 def pourDrink():
     clearScreen()
